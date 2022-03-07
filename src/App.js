ff9a622b78eca08d5d680636456445bb029e4b63
Rachel Sanham <green_chul@hotmail.com>
2022-03-07 20:00:40 +0000

    wip backend

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const userRouter = require('./routes/user');
 const app = express();
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors());
 
 app.post('/users', userRouter);
 
