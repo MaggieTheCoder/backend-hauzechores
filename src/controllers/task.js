26985949fad3970fc7f1a2bd4bc09ead9dcde092
MaggieTheCoder <mdurandt@gmail.com>
2022-03-11 15:31:30 +0200

    commented out gettaskcontroller

--- src/controllers/task.js
+++ src/controllers/task.js
@@ -31,28 +31,28 @@ exports.getAll = async (req, res) => {
   }
 };
 
-exports.getTaskByUserId = async (req, res) => {
-  const db = await getDb();
-  const userID = req.params.userID;
+// exports.getTaskByUserId = async (req, res) => {
+//   const db = await getDb();
+//   const userID = req.params.userID;
 
-  try {
-    const result = await db.query('SELECT * FROM Task WHERE userid=?', [
-      userID,
-    ]);
-    console.log({ userID });
-    const [[task]] = result;
-    console.log({ task });
-    if (task) {
-      res.status(200).send(task);
-    } else {
-      res.sendStatus(404);
-    }
-  } catch (error) {
-    console.log(error);
-    res.status(500).json(error);
-  }
-  db.close();
-};
+//   try {
+//     const result = await db.query('SELECT * FROM Task WHERE userid=?', [
+//       userID,
+//     ]);
+//     console.log({ userID });
+//     const [[task]] = result;
+//     console.log({ task });
+//     if (task) {
+//       res.status(200).send(task);
+//     } else {
+//       res.sendStatus(404);
+//     }
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json(error);
+//   }
+//   db.close();
+// };
 
 exports.updateTaskById = async (req, res) => {
   const db = await getDb();
--- src/routes/task.js
+++ src/routes/task.js
@@ -6,7 +6,7 @@ const taskRouter = express.Router();
 
 taskRouter.post('/tasks', taskController.create);
 taskRouter.get('/tasks', taskController.getAll);
-taskRouter.get('/tasks/userid/:userid', taskController.getTaskByUserId);
+// taskRouter.get('/tasks/userid/:userid', taskController.getTaskByUserId);
 taskRouter.patch('/tasks/:id', taskController.updateTaskById);
 taskRouter.delete('/tasks/:id', taskController.deleteById);
 
